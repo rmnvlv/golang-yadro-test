ad29c7c727882bd82d251e86f9d558140a12e356
rmnvlv <romario2000@bk.ru>
2024-05-18 18:59:25 +0300

    add: data writer

--- internal/writer.go
+++ internal/writer.go
@@ -1,3 +1,22 @@
 package internal
 
-func WriteData() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+func WriteData(data DataOut) {
+	fmt.Println(data.timeStart.Format("15:04"))
+	for _, s := range data.subjectsOut {
+		l := ""
+		if s.tableNumb != 0 {
+			l = strconv.Itoa(s.tableNumb)
+		}
+		fmt.Println(s.timeOfSubj.Format("15:04"), s.id, s.name, l)
+	}
+	fmt.Println(data.timeEnd.Format("15:04"))
+
+	for _, s := range data.payment {
+		fmt.Println(s.table, s.money, s.workingTime)
+	}
+}
